f03a1168e9889c4ab8ccac3d6ea6b8863cd0b7c4
NomadHash <badsoap141@gmail.com>
2021-04-23 15:14:38 +0900

    add next app

---
+++ index.js
@@ -0,0 +1,11 @@
+const program = require("commander");
+const pkg = require("./package.json");
+const open = require("open");
+
+const BASE_NPM_URL = "https://www.npmjs.com/package";
+
+program
+  .version(pkg.version)
+  .arguments("<module-name>")
+  .action((module) => open(`${BASE_NPM_URL}/${module}`, "Google chrome"))
+  .parse(process.argv);
